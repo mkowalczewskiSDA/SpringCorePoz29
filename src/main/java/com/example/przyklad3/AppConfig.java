d64304bcd4a57e34a6c5b2bc2c5f49bbba4e1bcd
Michal Kowalczewski <m.kowalczewski2@gmail.com>
2021-06-12 10:29:18 +0200

    przyklad3

--- src/main/java/com/example/przyklad3/AppConfig.java
+++ src/main/java/com/example/przyklad3/AppConfig.java
@@ -1,12 +1,14 @@
 package com.example.przyklad3;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ComponentScan("com.example.przyklad3")
 public class AppConfig {
 
-    @Bean
-    public HelloWorld helloWorld() { return new HelloWorld(); }
+    /*@Bean
+    public HelloWorld helloWorld() { return new HelloWorld(); }*/
 
 }

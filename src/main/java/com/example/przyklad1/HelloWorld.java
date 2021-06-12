c3d28371ae6b00cbe01a8bdd1e3f392c9e2ae678
Michal Kowalczewski <m.kowalczewski2@gmail.com>
2021-06-12 10:00:33 +0200

    przyklad1

---
+++ src/main/java/com/example/przyklad1/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.example.przyklad1;
+
+public class HelloWorld {
+
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
--- src/main/java/com/example/przyklad1/MainApp.java
+++ src/main/java/com/example/przyklad1/MainApp.java
@@ -1,4 +1,13 @@
 package com.example.przyklad1;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class MainApp {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext(
+                "przyklad1/Beans.xml");
+        HelloWorld helloWorld = (HelloWorld) context.getBean("myBean");
+        System.out.println(helloWorld.getMessage());
+    }
 }
